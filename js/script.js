9c0fb537f59a05892df55bc1838d7ca676724bc2
Julie Anderson <juliemanderson@gmail.com>
2016-07-22 14:54:41 -0500

    Add Scrollspy & Smooth Scrolling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+
+// Hides navdropdown after click
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
+
+
+// Enables smooth scrolling with offset
+var topoffset = 50;
+
+$('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      }
+    }
+  });
